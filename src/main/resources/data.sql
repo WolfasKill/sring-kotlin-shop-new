3b4bc884c9ef0e63328f61e435888407ad1393db
HYPERPC <serj5001@yandex.ru>
2025-04-24 17:05:32 +0300

    3 st

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,44 @@
-INSERT INTO TBL_PRODUCT (name, quantity) VALUES
-    ('ASUS', 10),
-    ('APLE', 11),
-    ('SAMSUNG', 12),
-    ('MAC', 2);
+-- INSERT INTO TBL_PRODUCT (name, quantity) VALUES
+--     ('ASUS', 10),
+--     ('APLE', 11),
+--     ('SAMSUNG', 12),
+--     ('MAC', 2);
+--
+--
+-- commit;
+--
+--
+-- SELECT * from TBL_PRODUCT;
 
+
+
+INSERT INTO GRP_PRODUCT (name) VALUES
+                                             ('TELEPHONE'),
+                                             ('PC'),
+                                             ('MONITOR'),
+                                             ('NOTEBOOK');
 
 commit;
 
 
-SELECT * from TBL_PRODUCT;
+INSERT INTO PRODUCT (grp_product_id,name) VALUES
+                                   (1,'APLE'),
+                                   (1,'SAMSUNG'),
+                                   (2,'HYPERPC'),
+                                   (3,'ASUS'),
+                                   (3,'SAMSUNG'),
+                                   (3,'LOC'),
+                                   (3,'DELL'),
+                                   (3,'ASUS'),
+                                   (3,'SAMSUNG')
 
 
+
+;
+
+commit;
+
+
+
+SELECT * from PRODUCT;
+SELECT * from GRP_PRODUCT;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,20 +1,28 @@
-DROP TABLE IF EXISTS TBL_PRODUCT;
+-- DROP TABLE IF EXISTS TBL_PRODUCT;
+--
+-- CREATE TABLE TBL_PRODUCT (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(250) NOT NULL,
+--     quantity INT NOT NULL
+-- );
+--
+-- commit;
 
 
+DROP TABLE IF EXISTS PRODUCT;
+DROP TABLE IF EXISTS GRP_PRODUCT;
 
 
-
-
-
-CREATE TABLE TBL_PRODUCT (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(250) NOT NULL,
-    quantity INT NOT NULL
+create table GRP_PRODUCT
+(
+    id         serial constraint country_pk primary key,
+    name       varchar not null
 );
 
-
-
+create table PRODUCT
+(
+    id         serial constraint city_pk primary key,
+    grp_product_id integer not null constraint city_country_fk references GRP_PRODUCT,
+    name       varchar not null
+);
 commit;
-
-
-
